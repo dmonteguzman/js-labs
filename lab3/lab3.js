fb312db899d346596e565053ef982442abb7ba26
Diego MG <dmonteguzman@gmail.com>
2018-03-28 15:30:35 +0200

    Finishing exercise of Lab3

---
+++ lab3/lab3.js
@@ -0,0 +1,9 @@
+let friends = ['Daniel', 'Carlos', 'Ricardo', 'Pepe', 'Sergio'];
+
+for (i=0; i<friends.length; i++){
+
+    for (j=100; j>0; j){
+        console.log(j + " lines of code in the file, " + j + " lines of code; " + friends[i] + " strikes one out, clears it all out, " + j-1 + " lines of code in the file");
+    }
+
+}
